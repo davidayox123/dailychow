3ea8c561467ef216311faab7f53fe2357770b294
davidayox123 <dayodele.2300823@stu.cu.edu.ng>
2025-06-21 09:43:55 +0100

    fully ready for production

---
+++ constants.py
@@ -0,0 +1,11 @@
+# ConversationHandler states
+SET_BUDGET_AMOUNT = 1
+TOPUP_AMOUNT_KORAPAY = 2
+SET_BANK_ACCOUNT_NUMBER = 3
+SET_BANK_BANK_CODE = 4
+ADD_MEAL_PLAN_DAY = 5
+ADD_MEAL_PLAN_DONE = 6
+
+# Budget limits
+MIN_BUDGET_AMOUNT = 1000  # Minimum allowed monthly budget (NGN)
+MAX_BUDGET_AMOUNT = 1000000  # Maximum allowed monthly budget (NGN)
